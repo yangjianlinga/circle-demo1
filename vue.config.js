70a62efae152ae45fdb30f0296011e8c212c50ac
FITOWSMART\yangjianling <yangjianling126@126.com>
2023-02-01 13:59:16 +0800

    udapte vueconfig

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/circle-demo':'/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/circle-demo1':'/'
 })
